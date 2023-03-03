53a2496072332d690954f944923bbe1083619462
yee882408 <saw634703@gmail.com>
2023-03-04 07:30:52 +0800

    Change6

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,66 +1,144 @@
 import View from './View';
 import icons from 'url:../../img/icons.svg';
 
-class PreviewView extends View {
-  _parentElement = '';
+import fracty from 'fracty';
 
-  // _generateMarkup() {
-  //   return this._data.map(this._generateMarkupPreview).join();
-  // }
+class RecipeView extends View {
+  _parentElement = document.querySelector('.recipe');
+  _errMsg = '找不到該食譜，請在試一次!';
+  _message = '';
 
-  // _generateMarkupPreview(result) {
-  //   const id = window.location.hash.slice(1);
-  //   return `
-  //          <li class="preview">
-  //           <a class="preview__link ${
-  //             result.id === id ? 'preview__link--active' : ''
-  //           }" href="#${result.id}">
-  //             <figure class="preview__fig">
-  //               <img src="${result.image}" alt="Test" />
-  //             </figure>
-  //             <div class="preview__data">
-  //               <h4 class="preview__title">${result.title}</h4>
-  //               <p class="preview__publisher">${result.publisher}</p>
-  //               <div class="preview__user-generated ${
-  //                 result.key ? '' : 'hidden'
-  //               }">
-  //                 <svg>
-  //                   <use href="${icons}#icon-user"></use>
-  //                 </svg>
-  //               </div>
-  //             </div>
-  //           </a>
-  //         </li>
-  //   `;
-  // }
+  addHandler(handler) {
+    ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, handler));
+  }
 
-  // When preview is sibling with resultview and bookmarkview can use this>>
+  addHandlerServings(handler) {
+    this._parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--update-servings');
+      if (!btn) return;
+
+      const { updateTo } = btn.dataset;
+
+      if (+updateTo > 0) handler(+updateTo);
+      // handler();
+    });
+  }
+
+  addHandlerAddBookmark(handler) {
+    this._parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--bookmark');
+      if (!btn) return;
+      handler();
+    });
+  }
+
   _generateMarkup() {
-    const id = window.location.hash.slice(1);
-
     return `
-      <li class="preview">
-          <a class="preview__link ${
-            this._data.id === id ? 'preview__link--active' : ''
-          }" href="#${this._data.id}">
-              <figure class="preview__fig">
-                  <img src="${this._data.image}" alt="${this._data.title}" />
-              </figure>
-              <div class="preview__data">
-                  <h4 class="preview__title">${this._data.title}</h4>
-                  <p class="preview__publisher">${this._data.publisher}</p>
-                  <div class="preview__user-generated ${
-                    this._data.key ? '' : 'hidden'
-                  }">
-                  <svg>
-                    <use href="${icons}#icon-user"></use>
-                  </svg>
-                </div>
-              </div>
-          </a>
-       </li>
+      <figure class="recipe__fig">
+        <img src="${this._data.image}" alt="${
+      this._data.title
+    }" class="recipe__img" />
+        <h1 class="recipe__title">
+          <span>${this._data.title}</span>
+        </h1>
+      </figure>
+
+      <div class="recipe__details">
+        <div class="recipe__info">
+          <svg class="recipe__info-icon">
+            <use href="${icons}#icon-clock"></use>
+          </svg>
+          <span class="recipe__info-data recipe__info-data--minutes">${
+            this._data.cookingTime
+          }</span>
+          <span class="recipe__info-text">分鐘</span>
+      </div>
+        <div class="recipe__info">
+          <svg class="recipe__info-icon">
+            <use href="${icons}#icon-users"></use>
+          </svg>
+          <span class="recipe__info-data recipe__info-data--people">${
+            this._data.servings
+          }</span>
+          <span class="recipe__info-text">人份</span>
+
+          <div class="recipe__info-buttons">
+            <button class="btn--tiny btn--update-servings" data-update-to="${
+              this._data.servings - 1
+            }">
+              <svg>
+                <use href="${icons}#icon-minus-circle"></use>
+              </svg>
+            </button>
+            <button class="btn--tiny btn--update-servings" data-update-to="${
+              this._data.servings + 1
+            }">
+              <svg>
+                <use href="${icons}#icon-plus-circle"></use>
+              </svg>
+            </button>
+          </div>
+        </div>
+
+      <div class="recipe__user-generated ${this._data.key ? '' : 'hidden'}">
+        <svg>
+          <use href="${icons}#icon-user"></use>
+        </svg>
+      </div>
+      <button class="btn--round btn--bookmark">
+        <svg class="">
+          <use href="${icons}#icon-bookmark${
+      this._data.bookmarked ? '-fill' : ''
+    }"></use>
+        </svg>
+      </button>
+    </div>
+
+    <div class="recipe__ingredients">
+      <h2 class="heading--2">食譜內容</h2>
+      <ul class="recipe__ingredient-list">
+        ${this._data.ingredients.map(this._generateMkuIng).join('')}
+      </ul>
+    </div>
+
+    <div class="recipe__directions">
+      <h2 class="heading--2">亨調方式</h2>
+      <p class="recipe__directions-text">
+      此食譜是由
+        <span class="recipe__publisher">${
+          this._data.publisher
+        }</span>. 所創造。歡迎點擊下方連結了解更多資訊。
+      </p>
+      <a
+        class="btn--small recipe__btn"
+        href="${this._data.sourceUrl}"
+        target="_blank"
+      >
+        <span>Directions</span>
+        <svg class="search__icon">
+          <use href="${icons}#icon-arrow-right"></use>
+        </svg>
+      </a>
+    </div>
   `;
+  }
+
+  _generateMkuIng(ing) {
+    return `
+            <li class="recipe__ingredient">
+              <svg class="recipe__icon">
+                <use href="${icons}#icon-check"></use>
+              </svg>
+              <div class="recipe__quantity">${
+                ing.quantity ? fracty(ing.quantity) : ''
+              }</div>
+              <div class="recipe__description">
+                <span class="recipe__unit">${ing.unit}</span>
+                ${ing.description}
+              </div>
+            </li>
+          `;
   }
 }
 
-export default new PreviewView();
+export default new RecipeView();
