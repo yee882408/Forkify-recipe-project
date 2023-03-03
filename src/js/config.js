eca773a4a52e400304c7f8152c97025790e0ac23
yee882408 <saw634703@gmail.com>
2023-03-04 07:21:45 +0800

    Change4

--- src/js/config.js
+++ src/js/config.js
@@ -2,4 +2,4 @@ export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = '314b1d43-eff7-4a49-b5d4-b79419a15091';
-export const MODAL_CLOSE_SEC = 2.5;
+export const MODEL_CLOSE_SEC = 2.5;
